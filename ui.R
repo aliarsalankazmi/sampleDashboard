4247424130c7c845dafb369b2cae82012bd44e7f
Ali Arsalan <scac_1041@hotmail.com>
2015-03-31 12:20:26 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,40 +1,12 @@
-#
-#
-## load libraries
+### load libraries
 
 library(shiny)
 
 
-## body for Shiny UI
+### body for Shiny UI
 
 
-shinyUI(fluidPage(
-
-title = 'My Dashboard',
-
-imageOutput('mainImg'),
-
-hr(),
-
-fluidRow(
-	column(2,
-		fileInput(inputId = 'mainFile', label = 'Select your file to upload', multiple = FALSE)
-		),
-	column(3,
-		h4('Cumulative Accrued Points'),
-		verbatimTextOutput('imgPath')#,
-#		plotOutput(...)
-		),
-	column(4,
-		h4('Cumulative Redeemed Points')#,
-#		verbatimTextOutput(...),
-#		plotOutput(...)
-		),
-	column(5,
-		h4('Cumulative Extra Metric')#,
-#		verbatimTextOutput(...),
-#		plotOutput(...)
-		)
-	)
-
-))
+shinyUI(navbarPage("Dashboard",
+  tabPanel("Details Section"),
+  tabPanel("Experiments Section"), inverse = TRUE)
+)
